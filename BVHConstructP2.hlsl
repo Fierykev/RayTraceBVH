4c317dae061177b88d6bbac106bc9ac6d22a88b8
Fierykev <cyberkpick@utexas.edu>
2017-10-06 18:42:41 -0500

    Added UINT_MAX

--- BVHConstructP2.hlsl
+++ BVHConstructP2.hlsl
@@ -8,7 +8,7 @@ MAY WANT TO LOOK FOR A LESS DIVERGENT ALGORITHM.
 [numthreads(DATA_SIZE, 1, 1)]
 void main(uint3 threadID : SV_DispatchThreadID, uint groupThreadID : SV_GroupIndex, uint3 groupID : SV_GroupID)
 {
-	int nodeID = BVHTree[threadID.x].parent;
+	uint nodeID = BVHTree[threadID.x].parent;
 
 	uint value;
 
@@ -24,12 +24,12 @@ void main(uint3 threadID : SV_DispatchThreadID, uint groupThreadID : SV_GroupInd
 
 		BVHTree[nodeID].bbox.bbMax = maxUnion(BVHTree[BVHTree[nodeID].childL].bbox.bbMax,
 			BVHTree[BVHTree[nodeID].childR].bbox.bbMax);
-		
+
 		// get the parent
 		nodeID = BVHTree[nodeID].parent;
 
 		// atomically add to prevent two threads from entering a node
-		if (nodeID != -1)
+		if (nodeID != UINT_MAX)
 			InterlockedAdd(transferBuffer[nodeID - numObjects], 1, value);
 		else
 			return;
